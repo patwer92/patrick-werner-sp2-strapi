43d2c29256c0aabead79d667cf4fffae52adf5cb
Patrick <31260874+patwer92@users.noreply.github.com>
2021-11-25 15:18:19 +0100

    Update database config

--- strapi-api/config/database.js
+++ strapi-api/config/database.js
@@ -1,31 +1,25 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "mongoose",
-      settings: {},
-      options: {},
-    },
-  },
-});
-
 // module.exports = ({ env }) => ({
 //   defaultConnection: "default",
 //   connections: {
 //     default: {
 //       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST"),
-//         srv: env.bool("DATABASE_SRV", true),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME"),
-//         username: env("DATABASE_USERNAME"),
-//         password: env("DATABASE_PASSWORD"),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", true),
-//       },
+//       settings: {},
+//       options: {},
 //     },
 //   },
 // });
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
